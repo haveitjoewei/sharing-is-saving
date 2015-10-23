d56e613771879444fdf6dd5f20f589335d37ffdd
haveitjoewei <weijoseph1025@gmail.com>
2015-10-23 06:45:36 -0700

    fix login and signup calls to api, refactor code to remove duplicate header, add background-image

--- scripts/share.js
+++ scripts/share.js
@@ -24,6 +24,12 @@ var Share = (function() {
     var makePostRequest = function(url, data, onSuccess, onFailure) {
         $.ajax({
             type: 'POST',
+            headers: {          
+               Accept : "application/json; charset=utf-8",         
+               "Content-Type": "application/json; charset=utf-8",  
+               "X-API-EMAIL" : readCookie('email'),
+               "X-API-TOKEN" : readCookie('auth_token')
+            },
             url: apiUrl + url,
             data: JSON.stringify(data),
             contentType: "application/json",
@@ -33,49 +39,65 @@ var Share = (function() {
         });
     };
 
+    function createCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    };
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    };
+
+    function deleteCookie(name) {
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
+
+
     var attachCreateHandler = function(e) {
     	create.on('click', '.submit-input', function(e) {
     		e.preventDefault();
     		
     		var post = {};
     		post["title"] = create.find('.title-input').val();
-    		post["latitude"] = parseInt(create.find('.latitude-input').val());
-    		post["longitude"] = parseInt(create.find('.longitude-input').val());
+    		post["latitude"] = 1 //parseInt(create.find('.latitude-input').val());
+    		post["longitude"] = 1 //parseInt(create.find('.longitude-input').val());
     		post["description"] = create.find('.description-input').val();
     		post["price"] = parseInt(create.find('.price-input').val());
-    		post["deposit"] = parseInt(create.find('.deposit-input').val());
-    		post["status"] = parseInt(create.find('.status-input').val());
-    		post["user"] = create.find('.user-input').val();
+    		post["security_deposit"] = parseInt(create.find('.deposit-input').val());
+    		post["status"] = 1 //parseInt(create.find('.status-input').val());
+    		post["user"] = 1 //create.find('.user-input').val();
 
-    		post = {
-    			"post": {
-    				"title": "example post 50",
-    				"latitude": "123.456",
-    				"longitude": 2345.67,
-    				"description": "example description",
-    				"price": 12.34,
-    				"security_deposit": 56.89,
-    				"user": 1,
-    				"status": 1
-    			}
-    		};
+            post = {"post": post};
 
-    		console.log(post);
+            // console.log(post);
+            // var x = document.cookie;
+            // console.log("cookie" + x);
 
     		var onSuccess = function(data) {
-    			// console.log(data);
+    			console.log(data);
+                window.location.href = 'listings.html';
     		};
 
     		var onFailure = function() {
     			console.error('create post failed');
     		};
 
-    		makePostRequest("/api/v1/users/1/posts", post, onSuccess, onFailure);
+    		makePostRequest("/api/v1/posts", post, onSuccess, onFailure);
     	});
     };
 
-	var displayPosts = function() {
-	};
 
 	var start = function() {
 		create = $(".create")
--- scripts/user.js
+++ scripts/user.js
@@ -3,6 +3,10 @@ var User = (function() {
 
 	var signup;
     var login;
+    var logout;
+    var signup_button;
+    var login_button;
+    var user_label;
 
 	var makeGetRequest = function(url, onSuccess, onFailure) {
        $.ajax({
@@ -25,10 +29,27 @@ var User = (function() {
     var makePostRequest = function(url, data, onSuccess, onFailure) {
         $.ajax({
             type: 'POST',
+            headers: {          
+               Accept : "application/json; charset=utf-8",         
+               "Content-Type": "application/json; charset=utf-8"   
+            },
             url: apiUrl + url,
             data: JSON.stringify(data),
             contentType: "application/json",
             dataType: "json",
+            success: onSuccess,
+            error: onFailure
+        });
+    };
+
+    var makeDeleteRequest = function(url, onSuccess, onFailure) {
+        $.ajax({
+            type: 'DELETE',
+            headers: {          
+               "X-API-EMAIL" : readCookie('email'),
+               "X-API-TOKEN" : readCookie('auth_token')
+            },
+            url: apiUrl + url,
             success: onSuccess,
             error: onFailure
         });
@@ -45,21 +66,52 @@ var User = (function() {
             user["first_name"] = signup.find('#first_name').val();
             user["last_name"] = signup.find('#last_name').val();
             user["date_of_birth"] = signup.find('#date_of_birth').val();
-            user["latitude"] = signup.find('#latitude').val();
-            user["longitude"] = signup.find('#longitude').val();
+            user["latitude"] = 1 //signup.find('#latitude').val();
+            user["longitude"] = 1 //signup.find('#longitude').val();
 
             user = {"user": user};
 
             var onSuccess = function(data) {
                 console.log(data);
+
+                var login_user = {};
+
+                login_user["email"] = data.user.email;
+                login_user["password"] = signup.find('#password').val();
+                console.log(login_user);
+
+                var onSuccess = function(data) {
+                    console.log(data);
+                    token = data.user.auth_token;
+                    email = data.user.email;
+                    createCookie('auth_token', token, 7);
+                    createCookie('email', email, 7);
+                    var x = readCookie('auth_token');
+                    var y = readCookie('email');
+                    console.log("cookie: " + x);
+                    console.log("cookie: " + y);
+                    user_label.show();
+                    user_label.text(data.user.email);
+                };
+
+                var onFailure = function(data) {
+                    console.log(data);
+                    console.error('login user failed');
+                }; 
+
+                makePostRequest("/api/v1/users/sign_in", login_user, onSuccess, onFailure);
+                signup_button.hide();
+                login_button.hide();
+                logout.show();
+                signup.hide();
             };
 
-            var onFailure = function() {
+            var onFailure = function(data) {
                 console.error('create user failed');
+                console.log(data);
             };
 
             makePostRequest("/api/v1/users", user, onSuccess, onFailure);
-            signup.hide();
         });
     };
 
@@ -68,14 +120,29 @@ var User = (function() {
             e.preventDefault();
 
             var user = {};
-            user["email"] = login.find('#email').val();
-            user["password"] = login.find('#password').val();
 
+            user["email"] = login.find('#loginemail').val();
+            user["password"] = login.find('#loginpassword').val();
+            
             var onSuccess = function(data) {
                 console.log(data);
+                token = data.user.auth_token;
+                email = data.user.email;
+                createCookie('auth_token', token, 7);
+                createCookie('email', email, 7);
+                var x = readCookie('auth_token');
+                var y = readCookie('email');
+                console.log("cookie: " + x);
+                console.log("cookie: " + y);
+                user_label.show();
+                user_label.text(data.user.email);
+                signup_button.hide();
+                login_button.hide();
+                logout.show();
             };
 
-            var onFailure = function() {
+            var onFailure = function(data) {
+                console.log(data);
                 console.error('login user failed');
             }; 
 
@@ -83,12 +150,67 @@ var User = (function() {
         });
     };
 
+    var attachLogoutHandler = function() {
+        logout.on('click', function(e){
+            e.preventDefault();
+
+            var onSuccess = function(data) {
+                console.log(data);
+                deleteCookie('auth_token');
+                deleteCookie('email');
+                // var x = document.cookie;
+                // console.log("cookie: " + x);
+                signup_button.show();
+                login_button.show();
+                logout.hide();
+                user_label.hide();
+            };
+
+            var onFailure = function(data) {
+                console.log(data);
+                console.log("damn");
+            };
+
+            makeDeleteRequest("/api/v1/users/sign_out", onSuccess, onFailure);
+        });
+    };
+
+    function createCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    };
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    };
+
+    function deleteCookie(name) {
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    };
+
 	var start = function() {
 		signup = $("#signup_dialog");
         login = $("#login_dialog");
+        logout = $("#logout");
+        signup_button = $("#signup");
+        login_button = $("#login");
+        user_label = $("#user");
 
 		attachSignupHandler();
         attachLoginHandler();
+        attachLogoutHandler();
 	};
 
 	return {
