dc9d0effc924159ab7cb992b20d4ad9599ff5b4d
Sidwyn Koh <sidwyn@gmail.com>
2015-11-06 15:17:22 -0800

    Exchange controller tests

--- spec/controllers/exchange_controller_spec.rb
+++ spec/controllers/exchange_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe ExchangeController, type: :controller do
+RSpec.describe ExchangesController, type: :controller do
+  include Devise::TestHelpers
+
+  describe "factory user" do
+    before :each do
+      @ma_user = FactoryGirl.create(:user)
+      @ma_user2 = FactoryGirl.create(:user2)
+      @post = FactoryGirl.create(:post)
+      sign_in @ma_user
+      byebug
+    end
+
+    it "should make a request for an exchange" do
+      byebug
+      post :create, { id: @post.id }
+      expect( response ).to redirect_to( new_user_session_path )
+    end
+  end
 
 end
