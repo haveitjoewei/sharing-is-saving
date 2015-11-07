9b98e7a799fc0045569817a1b0075c5f1101ae18
haveitjoewei <josephwei@berkeley.edu>
2015-11-06 22:36:52 -0800

    embed jquery to application.html.erb

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,36 +1,5 @@
 var ready = function() {
-  $("#signup_dialog").hide();
-  $("#login_dialog").hide();
-  $("#darken").hide();
-  $("#logout").hide();
-  $("#user").hide();
   $("#notificationContainer").hide();
-
-  $("#signup").click(function(){
-    $("#signup_dialog").fadeIn(100);
-    $("#darken").fadeIn(100);
-  });
-
-  $("#login").click(function(){
-    $("#login_dialog").fadeIn(100);
-    $("#darken").fadeIn(100);
-  });
-
-  $("#darken").click(function(){
-    $("#signup_dialog").fadeOut(100);
-    $("#login_dialog").fadeOut(100);
-    $("#darken").fadeOut(100);
-  });
-
-  $("#signup_submit").click(function(){
-    $("#signup_dialog").fadeOut(100);
-    $("#darken").fadeOut(100);
-  });
-
-  $("#login_submit").click(function(){
-    $("#login_dialog").fadeOut(100);
-    $("#darken").fadeOut(100);
-  });
 
   $("#notificationLink").click(function(){
       $("#notificationContainer").toggle();
