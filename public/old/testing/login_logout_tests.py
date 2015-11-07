b1c0cfc7954417aa427b2b717af94e2ae1451735
haveitjoewei <josephwei@berkeley.edu>
2015-11-06 21:39:54 -0800

    update more tests

--- public/old/testing/login_logout_tests.py
+++ public/old/testing/login_logout_tests.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 import unittest
 import time
 
-class LoginLogoutTestCase(unittest.TestCase):
+class Loginlogout_buttonTestCase(unittest.TestCase):
 	def setUp(self):
 		chrome_options = Options()
 		self.browser = webdriver.Chrome('./chromedriver')
@@ -11,74 +11,59 @@ class LoginLogoutTestCase(unittest.TestCase):
 		self.addCleanup(self.browser.quit)
 	def testPageTitle(self):
 		self.assertIn('Sharing Is Saving', self.browser.title)
-	def testDialog(self):
+	def testLogin(self):
 		self.browser.find_element_by_id('login').click()
-		self.assertTrue(self.browser.find_element_by_id('darken').is_displayed())
-	def testDarken(self):
-		self.browser.find_element_by_id('login').click()
-		self.assertTrue(self.browser.find_element_by_id('login_dialog').is_displayed())
-	def testLoginDarken(self):
-		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
 		time.sleep(2)
-		self.assertFalse(self.browser.find_element_by_id('darken').is_displayed())
-	def testLoginDialog(self):
-		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
+		self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+		self.browser.find_element_by_id('user_password').send_keys('password')
+		self.browser.find_element_by_name('commit').click()
 		time.sleep(2)
-		self.assertFalse(self.browser.find_element_by_id('login_dialog').is_displayed())
 	def testLoginUser(self):
 		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
 		time.sleep(2)
-		self.assertTrue(self.browser.find_element_by_id('user').is_displayed())
-	def testLoginLogout(self):
+		self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+		self.browser.find_element_by_id('user_password').send_keys('password')
+		self.browser.find_element_by_name('commit').click()
+		time.sleep(2)
+		self.assertTrue(self.browser.find_element_by_id('username').is_displayed())
+	def testLoginlogout_button(self):
 		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
 		time.sleep(2)
-		self.assertTrue(self.browser.find_element_by_id('logout').is_displayed())
-	def testLogoutLogin(self):
+		self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+		self.browser.find_element_by_id('user_password').send_keys('password')
+		self.browser.find_element_by_name('commit').click()
+		time.sleep(2)
+		self.assertTrue(self.browser.find_element_by_id('logout_button').is_displayed())
+	def testlogout_buttonLogin(self):
 		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
 		time.sleep(2)
-		self.browser.find_element_by_id('logout').click()
+		self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+		self.browser.find_element_by_id('user_password').send_keys('password')
+		self.browser.find_element_by_name('commit').click()
+		time.sleep(2)
+		self.browser.find_element_by_id('logout_button').click()
 		time.sleep(2)
 		self.assertTrue(self.browser.find_element_by_id('login').is_displayed())
-	def testLogoutUser(self):
-		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
-		time.sleep(2)
-		self.browser.find_element_by_id('logout').click()
-		time.sleep(2)
-		self.assertFalse(self.browser.find_element_by_id('user').is_displayed())
-	def testLogoutLogout(self):
-		self.browser.find_element_by_id('login').click()
-		self.browser.find_element_by_id('login_dialog').click()
-		self.browser.find_element_by_id('loginemail').send_keys('sidwyn@gmail.com')
-		self.browser.find_element_by_id('loginpassword').send_keys('password')
-		self.browser.find_element_by_id('login_submit').click()
-		time.sleep(2)
-		self.browser.find_element_by_id('logout').click()
-		time.sleep(2)
-		self.assertFalse(self.browser.find_element_by_id('logout').is_displayed())
+	# def testlogout_buttonUser(self):
+	# 	self.browser.find_element_by_id('login').click()
+	# 	time.sleep(2)
+	# 	self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+	# 	self.browser.find_element_by_id('user_password').send_keys('password')
+	# 	self.browser.find_element_by_name('commit').click()
+	# 	time.sleep(2)
+	# 	self.browser.find_element_by_id('logout_button').click()
+	# 	time.sleep(2)
+	# 	self.assertFalse(self.browser.find_element_by_id('username').is_displayed())
+	# def testlogout_buttonlogout_button(self):
+	# 	self.browser.find_element_by_id('login').click()
+	# 	time.sleep(2)
+	# 	self.browser.find_element_by_id('user_email').send_keys('sidwyn@gmail.com')
+	# 	self.browser.find_element_by_id('user_password').send_keys('password')
+	# 	self.browser.find_element_by_name('commit').click()
+	# 	time.sleep(2)
+	# 	self.browser.find_element_by_id('logout_button').click()
+	# 	time.sleep(2)
+	# 	self.assertFalse(self.browser.find_element_by_id('logout_button').is_displayed())
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
