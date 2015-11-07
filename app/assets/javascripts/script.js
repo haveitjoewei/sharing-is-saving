c00b6800a1db4f68807d55ad2b692d8b9ef6931c
haveitjoewei <josephwei@berkeley.edu>
2015-11-06 21:50:55 -0800

    fix jquery for notifications

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+var ready = function() {
   $("#signup_dialog").hide();
   $("#login_dialog").hide();
   $("#darken").hide();
@@ -32,24 +32,81 @@ $(document).ready(function(){
     $("#darken").fadeOut(100);
   });
 
-  $("#notificationLink").click(function()
-    {
-      $("#notificationContainer").fadeToggle(100);
-      $("#notification_count").fadeOut("slow");
-      return false;
-    });
+  $("#notificationLink").click(function(){
+      $("#notificationContainer").toggle();
+      // $("#notification_count").fadeOut("slow");
+      // return false;
+  });
 
-  // Document Click hiding the popup 
-  $(document).click(function()
-    {
-      $("#notificationContainer").hide();
-    });
+  // // Document Click hiding the popup 
+  // $(document).click(function()
+  //   {
+  //     $("#notificationContainer").hide();
+  //   });
 
-  // Popup on click
-  $("#notificationContainer").click(function()
-    {
-      return false;
-    });
+  // // Popup on click
+  // $("#notificationContainer").click(function()
+  //   {
+  //     return false;
+  //   });
+
+    // do stuff here.
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
+
+// $(document).ready(function(){
+//   $("#signup_dialog").hide();
+//   $("#login_dialog").hide();
+//   $("#darken").hide();
+//   $("#logout").hide();
+//   $("#user").hide();
+//   $("#notificationContainer").hide();
+
+//   $("#signup").click(function(){
+//     $("#signup_dialog").fadeIn(100);
+//     $("#darken").fadeIn(100);
+//   });
+
+//   $("#login").click(function(){
+//     $("#login_dialog").fadeIn(100);
+//     $("#darken").fadeIn(100);
+//   });
+
+//   $("#darken").click(function(){
+//     $("#signup_dialog").fadeOut(100);
+//     $("#login_dialog").fadeOut(100);
+//     $("#darken").fadeOut(100);
+//   });
+
+//   $("#signup_submit").click(function(){
+//     $("#signup_dialog").fadeOut(100);
+//     $("#darken").fadeOut(100);
+//   });
+
+//   $("#login_submit").click(function(){
+//     $("#login_dialog").fadeOut(100);
+//     $("#darken").fadeOut(100);
+//   });
+
+//   $("#notificationLink").click(function(){
+//       $("#notificationContainer").toggle();
+//       // $("#notification_count").fadeOut("slow");
+//       // return false;
+//   });
+
+//   // // Document Click hiding the popup 
+//   // $(document).click(function()
+//   //   {
+//   //     $("#notificationContainer").hide();
+//   //   });
+
+//   // // Popup on click
+//   // $("#notificationContainer").click(function()
+//   //   {
+//   //     return false;
+//   //   });
 
   
-});
+// });
